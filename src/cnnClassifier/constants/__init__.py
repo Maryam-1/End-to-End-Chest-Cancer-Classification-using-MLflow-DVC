be1ad2b047120efac82e75d2b763318a72fcb20c
maryam <zeeshan@MacBook.local>
2023-12-29 18:53:49 +0000

    data ingestion added

---
+++ src/cnnClassifier/constants/__init__.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("/Users/zeeshan 1/End-to-End-Chest-Cancer-Classification-using-MLflow-DVC/config/config.yaml")
+PARAMS_FILE_PATH = Path("/Users/zeeshan 1/End-to-End-Chest-Cancer-Classification-using-MLflow-DVC/params.yaml")
